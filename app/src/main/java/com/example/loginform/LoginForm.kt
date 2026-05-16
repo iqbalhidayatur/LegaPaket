644565722840165f4aca4de2a8d7a12c2ec65136
iqbalhidayatur <iqbalhidayaturr@gmail.com>
2026-05-16 20:51:12 +0700

    1

---
+++ app/src/main/java/com/example/loginform/LoginForm.kt
@@ -0,0 +1,14 @@
+package com.example.loginform
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        }
+    }
+}
